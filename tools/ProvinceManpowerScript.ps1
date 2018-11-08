86bf915f970eff6e4c891e3398fe7c24cb564029
bourbon <generalcharlesdegaulle1890@gmail.com>
2018-11-08 13:16:39 +0000

    added event giving USA the manpower back

---
+++ tools/ProvinceManpowerScript.ps1
@@ -0,0 +1,19 @@
+﻿$Path = Read-Host "Enter path to History/Provinces folder"
+$Tag = Read-Host "Enter Tag of target Country"
+$files = Get-ChildItem $Path -Recurse -File
+$MpPattern = "manpower(\ )?=(\ )?[0-9]+.[0-9]+"
+$TagPattern = "^controller(\ )?=(\ )?" + $Tag + "$"
+
+foreach($file in $files) {
+    $IsTag = Select-String -Path $file.PSPath -Pattern $TagPattern -Quiet
+    $HasManpower = Select-String -Path $file.PSPath -Pattern $MpPattern -Quiet
+    if ($HasManpower -and $IsTag) {
+        $mp = Select-String -Path $file.PSPath -Pattern $MpPattern | foreach{$_.Matches[0].value}
+        $loc = $file.Name | Select-String -Pattern "^[0-9]+" | foreach{$_.Matches[0].value}
+        $result = $loc + " = { change_" + $mp + " }"
+        Write-Host $result
+    }
+    foreach($line in Get-Content $file.PSPath) {
+        
+    }
+}
