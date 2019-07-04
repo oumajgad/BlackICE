7755be0ca779bd9a3c71fb0483ec7f821da77b43
oumajgad <ottoniusz@gmail.com>
2019-07-04 07:43:38 +0200

    Vichy joining factions fixed
    lowered CAG combat bonus

--- common/defines.lua
+++ common/defines.lua
@@ -258,7 +258,7 @@ military = {
 	AIR_UPGRADE_SPEED_MOD = 0.7, 
 	LAND_UPGRADE_SPEED_MOD = 0.7, --was 0.7
 	CAG_STACKING_PENALTY = 0.2,
-	CAG_SHIP_ATTACK_STR_BONUS = 16, -- attack bonus if CAG attacks ships who are busy in combat
+	CAG_SHIP_ATTACK_STR_BONUS = 8, -- attack bonus if CAG attacks ships who are busy in combat
 	CAG_SHIP_ATTACK_ORG_BONUS = 5,
 	NEW_LEADER_ORG_HIT = 0.3
 },
