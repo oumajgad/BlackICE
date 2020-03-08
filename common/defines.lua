4280246c1534fee50234c37615b9e337a92d6202
oumajgad <ottoniusz@gmail.com>
2020-03-08 23:15:49 +0100

    tweaks

--- common/defines.lua
+++ common/defines.lua
@@ -258,8 +258,8 @@ military = {
 	AIR_UPGRADE_SPEED_MOD = 1.1, 
 	LAND_UPGRADE_SPEED_MOD = 0.6, --was 0.7
 	CAG_STACKING_PENALTY = 0.2,
-	CAG_SHIP_ATTACK_STR_BONUS = 13, -- attack bonus if CAG attacks ships who are busy in combat
-	CAG_SHIP_ATTACK_ORG_BONUS = 5,
+	CAG_SHIP_ATTACK_STR_BONUS = 11, -- attack bonus if CAG attacks ships who are busy in combat
+	CAG_SHIP_ATTACK_ORG_BONUS = 5.5,
 	NEW_LEADER_ORG_HIT = 0.3
 },
 
