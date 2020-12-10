7c3f639882cc366f1fe99ef5a54826f46b7e7131
Dozed12 <up201607929@fe.up.pt>
2020-12-10 01:15:26 +0000

    PER AI trade oil from ENG perspective (Anglo-Persian Oil Company)

--- script/country/PER.lua
+++ script/country/PER.lua
@@ -11,4 +11,33 @@ function P.DiploScore_InviteToFaction(voDiploScoreObj)
 	return voDiploScoreObj.Score
 end
 
+function P.DiploScore_OfferTrade(voDiploScoreObj)
+
+	-- If trade involves Oil and Iran in Allies or no faction then handle it as if trading with ENG (Anglo-Persian Oil Company)
+	if voDiploScoreObj.ResourceRequest["vCrudeOil"] > 0 and 
+	(CCountryDataBase.GetTag("PER"):GetCountry():GetFaction() == CCurrentGameState.GetFaction("allies") or not CCountryDataBase.GetTag("ENG"):GetCountry():HasFaction()) then
+
+		local laTrade = {
+			ENG = {Score = 200},
+			CAN = {Score = 20},
+			AST = {Score = 20},
+			SAF = {Score = 20},
+			NZL = {Score = 20},
+			USA = {Score = 20},
+			CHI = {Score = 100},
+			GER = {Score = -20},
+			ITA = {Score = -20},
+			JAP = {Score = 10},
+			FRA = {Score = 20}}
+
+		if laTrade[voDiploScoreObj.TagName] then
+			return voDiploScoreObj.Score + laTrade[voDiploScoreObj.TagName].Score * 2
+		end
+
+	end
+
+	return voDiploScoreObj.Score
+
+end
+
 return AI_PER
