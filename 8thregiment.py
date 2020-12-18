3d73fe5b0a8d36477817899800d053e3931f3846
Dozed12 <up201607929@fe.up.pt>
2020-12-18 22:23:07 +0000

    Documentation 8thregiment script

--- 8thregiment.py
+++ 8thregiment.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 ######### INFO ##########
-# Detect oob files with divisions with 8 regiments
+# Detect oob files with divisions with 8 regiments in oob files and military_construction files
 #########################
 
 #Every province oob file
@@ -19,17 +19,21 @@ for txtPath, subdirs, files in os.walk("history/units/"):
                 lineN = 1
                 addedLine = False
                 for line in file:
+                    #Start count
                     if "division" in line or "corps" in line or "army" in line or "armygroup" in line or "theatre" in line or "military_construction" in line:
                         regCount = 0
                         addedLine = False
+                    #Count regiment or historical_model if not ship nor wing
                     if "regiment" in line or ("historical_model" in line and "ship" not in line and "wing" not in line):
                         regCount = regCount + 1
+                    #Detected 8th regiment and not repeat added line
                     if regCount == 8 and not addedLine:
                         regiment8 = regiment8 + 1
                         lines.append(lineN)
                         addedLine = True
                     lineN = lineN + 1
 
+                #Some 8th regiment was detected in file, print with line number
                 if regiment8 > 0:
                     txtfile = txtfile + os.path.join(txtPath, txt) + " - "
                     print(os.path.join(txtPath, txt) + " - ", flush=True, end="")
