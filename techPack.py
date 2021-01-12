ba87972c2fd7ce8bd9538aca593ccd823ec564e8
Dozed12 <up201607929@fe.up.pt>
2021-01-12 16:52:58 +0000

    Few tech packs and improved file placement

--- techPack.py
+++ techPack.py
@@ -17,110 +17,162 @@ packs = {}
 
 #Low Development (still over very low development countries that dont even have these)
 packs["low_dev"] = [ 
-    "basic_education = 2"
-    "civil_medicine = 3"
+    "basic_education = 2",
+    "civil_medicine = 3",
 
-    "industral_production = 1"
-    "coal_processing_technologies = 1"
-    "supply_production = 1"
+    "industral_production = 1",
+    "coal_processing_technologies = 1",
+    "supply_production = 1",
 
-    "construction_engineering = 1"
-    "industry_tech = 1"
-    "agriculture = 1"
+    "construction_engineering = 1",
+    "industry_tech = 1",
+    "agriculture = 1",
 
-    "mechanicalengineering_theory = 3"
-    "construction_practical = 3"
+    "mechanicalengineering_theory = 3",
+    "construction_practical = 3",
+]
+
+#Mine Development (countries rich in metals have developed it a bit)
+packs["mining"] = [
+    "coal_processing_technologies = 1",
+    "steel_production = 1",
+    "raremetal_refinning_techniques = 1",
+
+    "chemical_engineering = 4",
+]
+
+#Oil Development (countries rich in oil have developed it a bit)
+packs["oil"] = [
+    "oil_refinning = 1",
+
+    "chemical_engineering = 2",
+]
+
+#Automotive Industry (countries that have developed an automotive industry, ie.Romania)
+packs["automotive"] = [
+    "semi_motorization = 1",
+    "Vehicle_reliability = 1",
+    "motorcycle_recon_brigade_activation = 1",
+
+    "road_highway = 1",
 ]
 
 #Electronics edge  (developed electronics in Interwar)
 packs["electronic_edge"] = [
-    "electronic_mechanical_egineering = 1"
-    "radio_technology = 1"
+    "electronic_mechanical_egineering = 1",
+    "radio_technology = 1",
 
-    "electronic_engineering_theory = 3"
-    "electronic_engineering_practical = 1"
+    "electronic_engineering_theory = 3",
+    "electronic_engineering_practical = 1",
 ]
 
 #Mountain pack (countries with mountainous geography)
 packs["mountain"] = [
-    "mountain_infantry = 1"
-    "mountain_warfare_equipment = 1"
-    "mountain_training = 1"
-    "engineer_brigade_activation = 1"
-    "special_forces_upgrade = 1"
+    "mountain_infantry = 1",
+    "mountain_warfare_equipment = 1",
+    "mountain_training = 1",
+    "engineer_brigade_activation = 1",
+    "special_forces_upgrade = 1",
 ]
 
-#WW1 Experience (fought in WW1)
-packs["ww1"] = [
-    "civil_medicine = 3"
+#WW1 Experience (fought in WW1) (only doctrine and few advancements)
+packs["ww1_exp"] = [
+    "civil_medicine = 3",
 
-    "ww1_warfare = 2"
-    "artillery_barrage = 1"
+    "ww1_warfare = 2",
+    "artillery_barrage = 1",
+    "cavalry_pursuit_tactics = 1",
 
-    "brigade_command_structure = 1"
-    "divisonal_command_structure = 1"
+    "brigade_command_structure = 1",
+    "divisonal_command_structure = 1",
 
-    "infantry_training = 1"
-    "infantry_command_and_control = 1"
+    "infantry_training = 1",
+    "infantry_command_and_control = 1",
 
-    "ammo_production = 1"
+    "ammo_production = 1",
 
-    "infantry_theory = 2"
+    "infantry_theory = 2",
+]
+
+#Interwar Experience (fought during Interwar, ie.Poland) (doctrine and some equipment modernization)
+packs["interwar_exp"] = [
+    "smallarms_technology = 1",
+    "infantry_support = 1",
+    "infantry_guns = 1",
+    "infantry_at = 1",
+
+    "range_finding = 1",
+    "artillery_activation = 1",
+
+    "civil_medicine = 4",
+
+    "ww1_warfare = 3",
+    "artillery_barrage = 1",
+    "cavalry_pursuit_tactics = 2",
+
+    "brigade_command_structure = 1",
+    "divisonal_command_structure = 1",
+
+    "infantry_training = 1",
+    "infantry_command_and_control = 1",
+
+    "artillery_training = 1",
+
+    "ammo_production = 1",
+    "food_rations_production = 1",
+
+    "infantry_theory = 4",
+    "artillery_theory = 2",
 ]
 
 #WW1 Armor Experiments (experimented in WW1 armor)
 packs["ww1_armor"] = [
-    "rivetted_armour = 2"
+    "rivetted_armour = 2",
 
-    "steel_casting_capability = 1"
+    "steel_casting_capability = 1",
 
-    "infantry_tank_design = 1"
-    "armored_car_design = 1"
+    "infantry_tank_design = 1",
+    "armored_car_design = 1",
 
-    "automotive_theory = 2"
+    "automotive_theory = 2",
 ]
 
 #Interwar Armor Development (developed armor significantly in Interwar, ie. Sweden)
 packs["interwar_armor"] = [
-    "rivetted_armour = 5"
+    "rivetted_armour = 5",
 
-    "industral_production = 1"
-    "industral_efficiency = 1"
+    "industral_production = 1",
+    "industral_efficiency = 1",
 
-    "steel_casting_capability = 2"
-    "steel_electro_welding_technology = 1"
+    "steel_casting_capability = 2",
+    "steel_electro_welding_technology = 1",
 
-    "construction_engineering = 1"
-    "industry_tech = 2"
+    "construction_engineering = 1",
+    "industry_tech = 2",
 
-    "small_calibre_gun_design = 1"
+    "small_calibre_gun_design = 1",
 
-    "tank_chassis_design = 1"
+    "tank_chassis_design = 1",
 
-    "infantry_tank_design = 1"
-    "armored_car_design = 1"
+    "infantry_tank_design = 1",
+    "armored_car_design = 1",
 
-    "automotive_theory = 6"
-    "mechanicalengineering_theory = 5"
-    "construction_practical = 5"
+    "automotive_theory = 6",
+    "mechanicalengineering_theory = 5",
+    "construction_practical = 5",
 ]
 
 #Politically Agitated
 packs["political_agitation"] = [
-    "political_indoctrination = 1"
-    "political_integration = 1"
-    "partisan_suppression = 1"
-    "resistance_support = 1"
+    "political_indoctrination = 1",
+    "political_integration = 1",
+    "partisan_suppression = 1",
+    "resistance_support = 1",
 
-    "militia_theory = 3"
+    "militia_theory = 3",
 ]
 
 #TODO PACKS
-# Mining Industry
-# Oil Industry
-# Automotive Industry
-# WW1 Infantry
-# Interwar Infantry (countries that fought in Interwar ie. Poland)
 # WW1 Naval Basic (include heavy arty tech) (no AA ship tech)
 # WW1 Naval Capital (includes lighter ship tech) (include heavy arty tech) (no AA ship tech) (remember heavy armor forging)
 # Interwar Naval Basic (include heavy arty tech)
@@ -141,41 +193,34 @@ for path, subdirs, files in os.walk("history/countries/"):
                 #Selected pack
                 pack = packs[sys.argv[2]]
 
-                #Replace if existing
-                i = -1
+                #Remove pack parts already defined with higher value
                 for l in range(0,len(lines)):
+                    
+                    #Found date, break
+                    if lines[l].count(".") >= 2:
+                        #print(l)
+                        break
+
+                    #Ignore sub groups (ie. organization)
                     if len(lines[l].split("=")) != 2:
                         continue
+
                     for packPart in pack:
                         formatted = packPart.split("=")[0]
                         packValue = packPart.split("=")[1]
                         lineValue = lines[l].split("=")[1]
                         lineValue = lineValue.replace(" ","")
-                        #Replace if found and pack is higher value
                         if lineValue.isnumeric():
                             if formatted in lines[l]:
                                 if int(packValue) < int(lineValue):
                                     print("Found existing higher tech " + lines[l])
-                                    i = l
                                     pack.remove(packPart)
                                     break
-                                print("Found existing lower tech " + lines[l] + " => " + packPart)
-                                lines[l] = packPart
-                                i = l
-                                pack.remove(packPart)
-                                break
 
-                #Didnt find any replaced tech yet (use oob line)
-                if i == -1:
-                    for l in range(0,len(lines)):
-                        formatted = lines[l].split("=")[0]
-                        if "oob" in formatted:
-                            i = l
-                            break
-
-                #Write remaining pack techs
+                #Write tech pack at end of file
+                lines.append("\n# Tech Pack: " + sys.argv[2])
                 for packPart in pack:
-                    lines.insert(i, packPart)
+                    lines.append(packPart)
 
                 #Write new file
                 f = open(os.path.join(path, file), "r+")
