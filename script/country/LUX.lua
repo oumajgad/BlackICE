b6bf60a4930bd1bbda72b0aedf82d9199ba77573
Dozed12 <up201607929@fe.up.pt>
2021-01-28 03:06:04 +0000

    Update LUX.lua

--- script/country/LUX.lua
+++ script/country/LUX.lua
@@ -5,11 +5,24 @@ AI_LUX = P
 -- Production Weights
 --   1.0 = 100% the total needs to equal 1.0
 function P.ProductionWeights(voProductionData)
-	local laArray = {
-		0.0, -- Land
-		0.0, -- Air
-		0.0, -- Sea
-		1.0}; -- Other
+
+	local laArray
+
+	if voProductionData.IsAtWar then
+		-- War with Germany, all on land
+		laArray = {
+			1, -- Land
+			0.0, -- Air
+			0.0, -- Sea
+			0.0}; -- Other
+	else
+		-- Pre war
+		laArray = {
+			0.60, -- Land
+			0.30, -- Air
+			0.00, -- Sea
+			0.10}; -- Other
+	end
 	
 	return laArray
 end
