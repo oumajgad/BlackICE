53f75bae35e755e6f27b4f01c322f787d3a5a7b3
Dsafe1 <77176960+Dsafe1@users.noreply.github.com>
2021-01-31 12:55:15 +0100

    Create zipper.py

---
+++ zipper.py
@@ -0,0 +1,27 @@
+import os
+import zipfile
+import zlib
+import time
+
+
+
+Modfolders = ["./battleplans","./cgm","./common","./decisions","./events","./gfx","./history","./localisation","./map",
+                "./interface","./music","./script","./sound","./technologies","./units"]
+
+def zipdir(path):
+    for root, dirs, files in os.walk(path):
+        if root.split("\\")[0] not in Modfolders:
+            print("skipped " + root)
+            continue
+        else:
+            print("included " + root)
+            for file in files:
+                zipf.write(os.path.join(root, file))
+
+zipf = zipfile.ZipFile('BlackICE.zip', 'w', zipfile.ZIP_DEFLATED)
+zipdir('./')
+zipf.close()
+
+print("All done! :)" )
+print("This Terminal will self destruct in 5 seconds!")
+time.sleep(5)
