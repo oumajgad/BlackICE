7cea43325a7435205ae06452783557fe6a892e47
Dsafe1 <77176960+Dsafe1@users.noreply.github.com>
2021-02-11 22:14:40 +0100

    A to A Unit Upgrades

---
+++ UnitUpgrades.py
@@ -0,0 +1,17 @@
+import os
+
+#Generate Unit upgrades for A to A upgrade paths
+
+units = []
+
+with open("upgradeswhitelist.txt", "r") as file:
+    for line in file:
+        line = line.split(".")
+        units.append(line[0])
+
+
+print(units)
+with open("upgradesprint.txt", "w") as file1:
+    for unit in units:
+        #print(	"\tupgrade = { \n\t\tbase = " + unit + "\n\t\ttarget = " + unit + "\n\t}"
+        file1.write("\n\tupgrade = { \n\t\tbase = " + unit + "\n\t\ttarget = " + unit + "\n\t}")
