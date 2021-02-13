b8950dc9e6d24c5f8e0a45ff07da0368841f45d4
Dsafe1 <77176960+Dsafe1@users.noreply.github.com>
2021-02-13 21:13:43 +0100

    Update ProdQuUnits

--- ProdQuUnits.py
+++ ProdQuUnits.py
@@ -9,9 +9,8 @@ for root, dirs, files in os.walk(folder):
     for file in files:
         with open(os.path.join(root, file), "r", encoding="UTF-8", errors="ignore") as unit:
             for line in unit:
-                if file in aux:
+                if file in aux or "Flotilla" in file:
                     continue
-
                 elif "military_construction" in line:
                     if unit.name not in fList:
                         print(unit.name)
