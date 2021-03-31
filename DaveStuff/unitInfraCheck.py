c7897469aebe8dc975f45356334698a5affc16cd
Dsafe1 <77176960+Dsafe1@users.noreply.github.com>
2021-03-31 17:36:07 +0200

    Create unitInfraCheck.py

---
+++ DaveStuff/unitInfraCheck.py
@@ -0,0 +1,35 @@
+################################################################
+### Searches for Units that spawn in lvl 1/0 infra provinces ###
+################################################################
+
+import os
+
+folderUnits = "./history/units"
+folderProvinces = "./history/provinces"
+
+units = {}
+provinces = {}
+
+for root, dirs, files in os.walk(folderUnits):
+    for file in files:
+        with open(os.path.join(root, file), "r", encoding="UTF-8", errors="ignore") as unit:
+            for line in unit:
+                if "location" in line:
+                    location = int(line.split("location")[1].split("=")[1].split("#")[0].strip().split(" ")[0].split("\t")[0])
+                    units[unit.name] = location
+
+
+for root, dirs, files in os.walk(folderProvinces):
+    for file in files:
+        with open(os.path.join(root, file), "r", encoding="UTF-8", errors="ignore") as province:
+            for line in province:
+                if "infra" in line:
+                    infra = int(line.split("infra")[1].split("=")[1].split("#")[0].strip())
+                    if infra <= 1:
+                        provinces[province.name.split("\\")[2].strip()] = infra
+
+
+for infra in provinces:
+    for location in units:
+        if int(infra.split("-")[0].strip()) == units[location]:
+            print(location)
--- DaveStuffMain.py
+++ DaveStuffMain.py
@@ -17,19 +17,22 @@ def getExec(name):
 
 
 b_unitLeaders       = Button(main, text="unitLeaders", width=25, command= lambda: getExec("unitLeaders.py"))
-b_ProdQuUnits       = Button(main, text="ProdQuUnits", width=25, command= lambda: getExec("ProdQuUnits.py"))
 b_EventModifiers    = Button(main, text="EventModifiers", width=25, command= lambda: getExec("EventModifiers.py"))
+b_ProdQuUnits       = Button(main, text="ProdQuUnits", width=25, command= lambda: getExec("ProdQuUnits.py"))
 b_UnitUpgrades      = Button(main, text="UnitUpgrades", width=25, command= lambda: getExec("UnitUpgrades.py"))
 b_EventIDs          = Button(main, text="EventIDs", width=25, command= lambda: getExec("EventIDs.py"))
 b_countryStats      = Button(main, text="countryStats", width=25, command= lambda: getExec("countryStats.py"))
+b_unitInfraCheck      = Button(main, text="unitInfraCheck", width=25, command= lambda: getExec("unitInfraCheck.py"))
 
 b_unitLeaders.grid(row=0, column=0, padx=10, pady=10)
-b_ProdQuUnits.grid(row=1, column=0, padx=10, pady=10)
 b_EventModifiers.grid(row=0, column=1, padx=10, pady=10)
+b_ProdQuUnits.grid(row=1, column=0, padx=10, pady=10)
 b_UnitUpgrades.grid(row=1, column=1, padx=10, pady=10)
 b_EventIDs.grid(row=2, column=0, padx=10, pady=10)
 b_countryStats.grid(row=2, column=1, padx=10, pady=10)
+b_unitInfraCheck.grid(row=3, column=0, padx=10, pady=10)
 
 
 
 main.mainloop()
+
