e2d3416ef835b9152a0729365661b672379a87b2
Dsafe1 <77176960+Dsafe1@users.noreply.github.com>
2021-05-10 20:33:35 +0200

    🐍

---
+++ DaveStuff/flagCheck.py
@@ -0,0 +1,55 @@
+import os
+
+
+### Looks for flags that get checked but are never set
+
+
+folders = ["./events" , "./decisions"]
+
+setflags = []
+searchedflags = []
+
+i = 0
+j = 0
+
+for folder in folders:
+    for root, dirs, files in os.walk(folder):
+        for file1 in files:
+            with open(root + "/" + file1, "r" , errors="ignore") as file:
+                #print(file.name)
+                for line in file:
+                    if "set_country_flag" in line and not line.strip().startswith("#"):
+                        i += 1
+                        flag = line.split("set_country_flag")[1].split("=")[1].strip().split(" ")[0].replace("}","").strip().lower()
+                        #print(flag)
+                        setflags.append(flag)
+                        #if i % 10 == 0:
+                            #os.system("pause")
+
+            
+for folder in folders:
+    for root, dirs, files in os.walk(folder):
+        for file1 in files:
+            with open(root + "/" + file1, "r" , errors="ignore") as file:
+                #print(file.name)
+                for line in file:
+                    if "has_country_flag" in line and not line.strip().startswith("#"):
+                        j += 1
+                        flag = line.split("has_country_flag")[1].split("=")[1].strip().split(" ")[0].replace("}","").strip().lower()
+                        #print(flag)
+                        searchedflags.append(flag)
+                        #if flag == "4":
+                            #print(line.split("has_country_flag")[1].split("=")[1].strip().split(" ")[0].replace("}","").strip().lower())
+                            #os.system("pause")
+                        #if j % 10 == 0:
+                            #os.system("pause")
+
+#remove duplicates from the lists
+setflagsclean = list(dict.fromkeys(setflags))
+searchedflagsclean = list(dict.fromkeys(searchedflags))
+
+print("Flags checked, but not set!\n")
+
+for searchedflag in searchedflagsclean:
+    if searchedflag not in setflagsclean:
+        print(searchedflag)
--- DaveStuffMain.py
+++ DaveStuffMain.py
@@ -25,6 +25,7 @@ b_countryStats      = Button(main, text="countryStats", width=25, command= lambd
 b_unitInfraCheck    = Button(main, text="unitInfraCheck", width=25, command= lambda: runExec("unitInfraCheck.py"))
 b_techParser        = Button(main, text="techParser", width=25, command= lambda: runExec("techParser.py"))
 b_modelBuilder      = Button(main, text="modelBuilder", width=25, command= lambda: runExec("modelBuilder.py"))
+b_flagCheck         = Button(main, text="flagCheck", width=25, command= lambda: runExec("flagCheck.py"))
 
 b_unitLeaders.grid          (row=0, column=0, padx=10, pady=10)
 b_EventModifiers.grid       (row=0, column=1, padx=10, pady=10)
@@ -35,6 +36,7 @@ b_countryStats.grid         (row=2, column=1, padx=10, pady=10)
 b_unitInfraCheck.grid       (row=3, column=0, padx=10, pady=10)
 b_techParser.grid           (row=3, column=1, padx=10, pady=10)
 b_modelBuilder.grid         (row=4, column=0, padx=10, pady=10)
+b_flagCheck.grid            (row=4, column=1, padx=10, pady=10)
 
 
 main.mainloop()
