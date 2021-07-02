e81f1bced2cbf74b95ed18a24d86f9d022b17bb3
Dsafe1 <77176960+Dsafe1@users.noreply.github.com>
2021-07-02 18:13:28 +0200

    🐍

--- zipper.py
+++ zipper.py
@@ -16,14 +16,23 @@ if x.lower() == "y":
     Modfolders.append("./gfx")
     filename = "BlackIceGFX.zip"
 
+maxcount = 0
+
+for root, dirs, files in os.walk("./"):
+    for file in files:
+        if root.split("\\")[0] in Modfolders:
+            maxcount +=1
+
 def zipdir(path):
+    counter = 0
     for root, dirs, files in os.walk(path):
         if root.split("\\")[0] not in Modfolders:
-            print("skipped " + root )
+            print(str(counter) + " - " + str(maxcount) +  "  skipped " + root )
             continue
         else:
             for file in files:
-                print("included " + str(root) + "/" + str(file))
+                print(str(counter) + " - " + str(maxcount) +  "  included " + str(root) + "/" + str(file))
+                counter +=1
                 zipf.write(os.path.join(root, file))
 
 zipf = zipfile.ZipFile(filename, 'w', zipfile.ZIP_DEFLATED)
