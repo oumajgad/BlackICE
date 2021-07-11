2064cf830c172faa8086df3934aa32a03579540c
Dsafe1 <77176960+Dsafe1@users.noreply.github.com>
2021-07-11 19:16:21 +0200

    KOR start with alliance with JAP; add KOR AI file hopefully fixes sleeping AI; POR doesnt give Azores if in axis

---
+++ script/country/KOR.lua
@@ -0,0 +1,108 @@
+
+local P = {}
+AI_KOR = P
+
+-- Tech weights
+--   1.0 = 100% the total needs to equal 1.0
+function P.TechWeights(voTechnologyData)
+	local laTechWeights = {
+		0.30, -- landBasedWeight
+		0.20, -- landDoctrinesWeight
+		0.0, -- airBasedWeight
+		0.0, -- airDoctrinesWeight
+		0.0, -- navalBasedWeight
+		0.0, -- navalDoctrinesWeight
+		0.50, -- industrialWeight
+		0.0, -- secretWeaponsWeight
+		0.0}; -- otherWeight
+
+	return laTechWeights
+end
+
+function P.AirTechs(voTechnologyData)
+	local ignoreTech = {"all"};
+
+	return ignoreTech, nil
+end
+
+function P.AirDoctrineTechs(voTechnologyData)
+	local ignoreTech = {"all"};
+
+	return ignoreTech, nil
+end
+
+function P.NavalTechs(voTechnologyData)
+	local ignoreTech = {"all"}
+
+	return ignoreTech, nil
+end
+
+function P.NavalDoctrineTechs(voTechnologyData)
+	local ignoreTech = {"all"};
+
+	return ignoreTech, nil
+end
+
+function P.SecretWeaponTechs(voTechnologyData)
+	local ignoreTech = {"all"}
+
+	return ignoreTech, nil
+end
+
+-- END OF TECH RESEARCH OVERIDES
+-- #######################################
+
+
+-- #######################################
+-- Production Overides the main LUA with country specific ones
+
+-- Land ratio distribution
+function P.LandRatio(voProductionData)
+	local laArray
+	laArray = {
+		infantry_bat = 1,
+		garrison_brigade = 4,
+		militia_brigade = 5,
+		cavalry_brigade = 3};
+
+	return laArray
+end
+
+-- Air ratio distribution
+function P.AirRatio(voProductionData)
+	local laArray = {
+		interceptor = 1};
+
+	return laArray
+end
+
+-- Transport to Land unit distribution
+-- UNUSED
+function P.TransportLandRatio(voProductionData)
+	local laArray = {
+		0, -- Land
+		0,  -- transport
+		0}  -- invasion craft
+
+	return laArray
+end
+
+-- Convoy Ratio control
+--- NOTE: If goverment is in Exile these parms are ignored
+function P.ConvoyRatio(voProductionData)
+	local laArray = {
+		0, -- Percentage extra (adds to 100 percent so if you put 10 it will make it 110% of needed amount)
+		5, -- If Percentage extra is less than this it will force it up to the amount entered
+		10, -- If Percentage extra is greater than this it will force it down to this
+		0} -- Escort to Convoy Ratio (Number indicates how many convoys needed to build 1 escort)
+
+	return laArray
+end
+
+-- Do not build coastal forts
+
+
+-- END OF PRODUTION OVERIDES
+-- #######################################
+
+return AI_KOR
