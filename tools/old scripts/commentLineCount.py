d31c498117788675af531f6ea13c61defcd67654
Dsafe1 <77176960+Dsafe1@users.noreply.github.com>
2021-09-20 16:34:47 +0200

    Update commentLineCount.py

--- tools/old scripts/commentLineCount.py
+++ tools/old scripts/commentLineCount.py
@@ -2,14 +2,13 @@ import os
 
 
 
-folders = ["./decisions", "./events"]
-
 found_list = []
 
-for folder in folders:
-    for path, subdirs, files in os.walk(folder):
-        for file in files:
+for path, subdirs, files in os.walk("./"):
+    for file in files:
+        if ".txt" in file:
             with open(os.path.join(path, file), "r", encoding="Windows-1252", errors="ignore") as file0:
+                print(file0.name)
                 line_nr = 0
                 counter = 0
                 found = False
@@ -26,12 +25,12 @@ for folder in folders:
                         found = False
                         counter = 0
             if counter >= 100:
-                found_list.append([file0.name, counter, line_nr])
+                found_list.append((file0.name, counter, line_nr))
             found = False
             counter = 0
 
-
-for element in found_list:
+found_list_sorted = sorted(found_list, key=lambda count:count[1])
+for element in found_list_sorted:
     print("file\t - \t" + str(element[0]))
     print("count\t - \t" + str(element[1]))
     print("line\t - \t" + str(element[2]))
