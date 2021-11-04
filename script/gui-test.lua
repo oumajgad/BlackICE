8aa0651b9bb64239b307bfe9b6c1eb865edf7f75
Dsafe1 <77176960+Dsafe1@users.noreply.github.com>
2021-11-04 16:05:58 +0100

    Update gui-test.lua

--- script/gui-test.lua
+++ script/gui-test.lua
@@ -1,3 +1,7 @@
+------------------------------------------------------
+-- https://wxlua.sourceforge.net/docs/wxluaref.html --
+------------------------------------------------------
+
 package.cpath = package.cpath..";./tfh/mod/?.dll;"
 require("wx")
 
@@ -7,48 +11,69 @@ if wx ~= nil then -- wx is not initialized until a few ticks have gone by. Do th
     frame = wx.wxFrame(wx.NULL, wx.wxID_ANY, "Hoi3 Utility",
                         wx.wxDefaultPosition, wx.wxDefaultSize)
 
+    notebook = wx.wxNotebook(frame, wx.wxID_ANY, wx.wxDefaultPosition, wx.wxDefaultSize)
+
     -- Panel = The canvas on which we build our gui.
-    panel = wx.wxPanel(frame, wx.wxID_ANY)
+    panel = wx.wxPanel(notebook, wx.wxID_ANY)
 
     -- Sizers take care of the arrangement and size (duh) of our buttons and/or the whole window/panel.
-    local mainSizer = wx.wxBoxSizer(wx.wxVERTICAL)
-    local staticBox      = wx.wxStaticBox(panel, wx.wxID_ANY, "Cool Text")
-    local staticBoxSizer = wx.wxStaticBoxSizer(staticBox, wx.wxVERTICAL)
-    local flexGridSizer  = wx.wxFlexGridSizer( 0, 3, 0, 0 )
-    flexGridSizer:AddGrowableCol(1, 0)
+    local mainSizer         = wx.wxBoxSizer(wx.wxVERTICAL)
+    local staticBox         = wx.wxStaticBox(panel, wx.wxID_ANY, "Cool Text")
+    local staticBoxSizer    = wx.wxStaticBoxSizer(staticBox, wx.wxVERTICAL)
+    local flexGridSizer     = wx.wxFlexGridSizer( 2, 2, 3, 3 )
 
-    staticBoxSizer:Add( flexGridSizer,  0, wx.wxGROW+wx.wxALIGN_CENTER+wx.wxALL, 5 )
-    mainSizer:Add(      staticBoxSizer, 1, wx.wxGROW+wx.wxALIGN_CENTER+wx.wxALL, 5 )
 
     button_left_ID = 1
     button_right_ID = 2
+    button_3_ID = 3
+    button_4_ID = 4
+    button_5_ID = 5
+    button_6_ID = 6
 
-    local buttonSizer = wx.wxBoxSizer( wx.wxHORIZONTAL )
     local ButtonLeft = wx.wxButton( panel, button_left_ID, "Almost Daily Resource Count OFF")
     local ButtonRight = wx.wxButton( panel, button_right_ID, "Almost Daily Resource Count ON")
-    flexGridSizer:Add( ButtonLeft, 0, wx.wxALIGN_CENTER+wx.wxALL, 5 )
-    buttonSizer:Add( ButtonRight, 0, wx.wxALIGN_CENTER+wx.wxALL, 5 )
+    local Button3 = wx.wxButton( panel, button_3_ID, "333")
+    local Button4 = wx.wxButton( panel, button_4_ID, "444")
+    local Button5 = wx.wxButton( panel, button_5_ID, "555")
+    local Button6 = wx.wxButton( panel, button_6_ID, "666")
+    flexGridSizer:Add( ButtonLeft, 0, wx.wxALIGN_CENTER+wx.wxALL, 0 )
+    flexGridSizer:Add( ButtonRight, 0, wx.wxALIGN_CENTER+wx.wxALL, 50 )
+    flexGridSizer:Add( Button3, 0, wx.wxALIGN_CENTER+wx.wxALL, 0 )
+    flexGridSizer:Add( Button4, 0, wx.wxALIGN_CENTER+wx.wxALL, 0 )
+    flexGridSizer:Add( Button5, 0, wx.wxALIGN_CENTER+wx.wxALL, 0 )
+    flexGridSizer:Add( Button6, 0, wx.wxALIGN_CENTER+wx.wxALL, 0 )
 
-    mainSizer:Add(   buttonSizer, 0, wx.wxALIGN_CENTER+wx.wxALL, 5 )
 
+    staticBoxSizer:Add( flexGridSizer,  0, wx.wxGROW+wx.wxALIGN_CENTER+wx.wxALL, 5 )
+    mainSizer:Add(   staticBoxSizer, 0, wx.wxALIGN_CENTER+wx.wxALL, 10 )
+
+    notebook:AddPage(panel, "Page 1")
 
     panel:SetSizer( mainSizer )
     mainSizer:SetSizeHints( frame )
+    frame:SetSizeHints(notebook:GetBestSize():GetWidth(), notebook:GetBestSize():GetHeight())
 
 
     panel:Connect(button_left_ID, wx.wxEVT_COMMAND_BUTTON_CLICKED,
         function(event)
-            DateOverride = false
-            Utils.LUA_DEBUGOUT(tostring(DateOverride))
+            if saveLoaded == true then
+                DateOverride = false
+                Utils.LUA_DEBUGOUT(tostring(DateOverride))
+            else
+                Utils.LUA_DEBUGOUT("NO SAVE LOADED")
+            end
         end)
     --
     panel:Connect(button_right_ID, wx.wxEVT_COMMAND_BUTTON_CLICKED,
         function(event)
-            DateOverride = true
-            Utils.LUA_DEBUGOUT(tostring(DateOverride))
+            if saveLoaded == true then
+                DateOverride = true
+                Utils.LUA_DEBUGOUT(tostring(DateOverride))
+            else
+                Utils.LUA_DEBUGOUT("NO SAVE LOADED")
+            end
         end)
     --
-
 
     frame:Centre()
     frame:Show(true)
