7406361bafacc77de8f49938bbd0a8aa40efa5db
Dsafe1 <77176960+Dsafe1@users.noreply.github.com>
2021-11-08 16:20:16 +0100

    Update gui-test.lua

--- script/gui-test.lua
+++ script/gui-test.lua
@@ -9,16 +9,16 @@ if wx ~= nil then -- wx is not initialized until a few ticks have gone by. Do th
 
     -- Frame = Literally just the window. Program name, minimize, maximize, close. Thats it.
     frame = wx.wxFrame(wx.NULL, wx.wxID_ANY, "Hoi3 Utility",
-                        wx.wxDefaultPosition, wx.wxDefaultSize)
+                        wx.wxDefaultPosition, wx.wxSize(500, 500))
 
-    notebook = wx.wxNotebook(frame, wx.wxID_ANY, wx.wxDefaultPosition, wx.wxDefaultSize)
+    notebookActions = wx.wxNotebook(frame, wx.wxID_ANY, wx.wxDefaultPosition, wx.wxDefaultSize)
 
     -- Panel = The canvas on which we build our gui.
-    panel = wx.wxPanel(notebook, wx.wxID_ANY)
+    panelActions = wx.wxPanel(notebookActions, wx.wxID_ANY)
 
     -- Sizers take care of the arrangement and size (duh) of our buttons and/or the whole window/panel.
     local mainSizer         = wx.wxBoxSizer(wx.wxVERTICAL)
-    local staticBox         = wx.wxStaticBox(panel, wx.wxID_ANY, "Cool Text")
+    local staticBox         = wx.wxStaticBox(panelActions, wx.wxID_ANY, "Cool Text")
     local staticBoxSizer    = wx.wxStaticBoxSizer(staticBox, wx.wxVERTICAL)
     local flexGridSizer     = wx.wxFlexGridSizer( 2, 2, 3, 3 )
 
@@ -30,12 +30,12 @@ if wx ~= nil then -- wx is not initialized until a few ticks have gone by. Do th
     button_5_ID = 5
     button_6_ID = 6
 
-    local ButtonLeft = wx.wxButton( panel, button_left_ID, "Almost Daily Resource Count OFF")
-    local ButtonRight = wx.wxButton( panel, button_right_ID, "Almost Daily Resource Count ON")
-    local Button3 = wx.wxButton( panel, button_3_ID, "333")
-    local Button4 = wx.wxButton( panel, button_4_ID, "444")
-    local Button5 = wx.wxButton( panel, button_5_ID, "555")
-    local Button6 = wx.wxButton( panel, button_6_ID, "666")
+    local ButtonLeft = wx.wxButton( panelActions, button_left_ID, "Almost Daily Resource Count OFF")
+    local ButtonRight = wx.wxButton( panelActions, button_right_ID, "Almost Daily Resource Count ON")
+    local Button3 = wx.wxButton( panelActions, button_3_ID, "333")
+    local Button4 = wx.wxButton( panelActions, button_4_ID, "444")
+    local Button5 = wx.wxButton( panelActions, button_5_ID, "555")
+    local Button6 = wx.wxButton( panelActions, button_6_ID, "666")
     flexGridSizer:Add( ButtonLeft, 0, wx.wxALIGN_CENTER+wx.wxALL, 0 )
     flexGridSizer:Add( ButtonRight, 0, wx.wxALIGN_CENTER+wx.wxALL, 50 )
     flexGridSizer:Add( Button3, 0, wx.wxALIGN_CENTER+wx.wxALL, 0 )
@@ -47,33 +47,90 @@ if wx ~= nil then -- wx is not initialized until a few ticks have gone by. Do th
     staticBoxSizer:Add( flexGridSizer,  0, wx.wxGROW+wx.wxALIGN_CENTER+wx.wxALL, 5 )
     mainSizer:Add(   staticBoxSizer, 0, wx.wxALIGN_CENTER+wx.wxALL, 10 )
 
-    notebook:AddPage(panel, "Page 1")
+    notebookActions:AddPage(panelActions, "Page 1")
+    panelActions:SetSizer( mainSizer )
+    -- Couldn't get the dynamic sizing to work... so just set statics during the frame init
+    -- mainSizer:SetSizeHints( frame )
+    -- frame:SetSizeHints(notebook:GetBestSize():GetWidth(), notebook:GetBestSize():GetHeight())
 
-    panel:SetSizer( mainSizer )
-    mainSizer:SetSizeHints( frame )
-    frame:SetSizeHints(notebook:GetBestSize():GetWidth(), notebook:GetBestSize():GetHeight())
-
-
-    panel:Connect(button_left_ID, wx.wxEVT_COMMAND_BUTTON_CLICKED,
+    panelActions:Connect(button_left_ID, wx.wxEVT_COMMAND_BUTTON_CLICKED,
         function(event)
             if saveLoaded == true then
                 DateOverride = false
-                Utils.LUA_DEBUGOUT(tostring(DateOverride))
+                --Utils.LUA_DEBUGOUT(tostring(DateOverride))
             else
-                Utils.LUA_DEBUGOUT("NO SAVE LOADED")
+                --Utils.LUA_DEBUGOUT("NO SAVE LOADED")
             end
         end)
     --
-    panel:Connect(button_right_ID, wx.wxEVT_COMMAND_BUTTON_CLICKED,
+    panelActions:Connect(button_right_ID, wx.wxEVT_COMMAND_BUTTON_CLICKED,
         function(event)
             if saveLoaded == true then
                 DateOverride = true
-                Utils.LUA_DEBUGOUT(tostring(DateOverride))
+                --Utils.LUA_DEBUGOUT(tostring(DateOverride))
             else
-                Utils.LUA_DEBUGOUT("NO SAVE LOADED")
+                --Utils.LUA_DEBUGOUT("NO SAVE LOADED")
             end
         end)
     --
+
+    panelPuppet = wx.wxPanel(notebookActions, wx.wxID_ANY)
+
+    -- Sizers take care of the arrangement and size (duh) of our buttons and/or the whole window/panel.
+    local mainSizerPuppet         = wx.wxBoxSizer(wx.wxVERTICAL)
+    local staticBoxPuppet         = wx.wxStaticBox(panelPuppet, wx.wxID_ANY, "Cool Text")
+    local staticBoxSizerPuppet    = wx.wxStaticBoxSizer(staticBoxPuppet, wx.wxVERTICAL)
+    local flexGridSizerPuppet     = wx.wxFlexGridSizer( 2, 2, 3, 3 )
+
+
+    button_21_ID = 21
+    button_22_ID = 22
+    button_23_ID = 23
+    button_24_ID = 24
+    button_25_ID = 25
+    button_26_ID = 26
+
+    local Button1 = wx.wxButton( panelPuppet, button_21_ID, "111")
+    local Button2 = wx.wxButton( panelPuppet, button_22_ID, "222")
+    local Button3 = wx.wxButton( panelPuppet, button_23_ID, "333")
+    local Button4 = wx.wxButton( panelPuppet, button_24_ID, "444")
+    local Button5 = wx.wxButton( panelPuppet, button_25_ID, "555")
+    local Button6 = wx.wxButton( panelPuppet, button_26_ID, "666")
+    flexGridSizerPuppet:Add( Button1, 0, wx.wxALIGN_CENTER+wx.wxALL, 0 )
+    flexGridSizerPuppet:Add( Button2, 0, wx.wxALIGN_CENTER+wx.wxALL, 50 )
+    flexGridSizerPuppet:Add( Button3, 0, wx.wxALIGN_CENTER+wx.wxALL, 0 )
+    flexGridSizerPuppet:Add( Button4, 0, wx.wxALIGN_CENTER+wx.wxALL, 0 )
+    flexGridSizerPuppet:Add( Button5, 0, wx.wxALIGN_CENTER+wx.wxALL, 0 )
+    flexGridSizerPuppet:Add( Button6, 0, wx.wxALIGN_CENTER+wx.wxALL, 0 )
+
+
+    staticBoxSizerPuppet:Add( flexGridSizerPuppet,  0, wx.wxGROW+wx.wxALIGN_CENTER+wx.wxALL, 5 )
+    mainSizerPuppet:Add(   staticBoxSizerPuppet, 0, wx.wxALIGN_CENTER+wx.wxALL, 10 )
+
+    notebookActions:AddPage(panelPuppet, "Page 2")
+    panelPuppet:SetSizer( mainSizerPuppet )
+
+    panelPuppet:Connect(button_21_ID, wx.wxEVT_COMMAND_BUTTON_CLICKED,
+        function(event)
+            if saveLoaded == true then
+                DateOverride = false
+                --Utils.LUA_DEBUGOUT(tostring(DateOverride))
+            else
+                --Utils.LUA_DEBUGOUT("NO SAVE LOADED")
+            end
+        end)
+    --
+    panelPuppet:Connect(button_22_ID, wx.wxEVT_COMMAND_BUTTON_CLICKED,
+        function(event)
+            if saveLoaded == true then
+                DateOverride = true
+                --Utils.LUA_DEBUGOUT(tostring(DateOverride))
+            else
+                --Utils.LUA_DEBUGOUT("NO SAVE LOADED")
+            end
+        end)
+    --
+
 
     frame:Centre()
     frame:Show(true)
