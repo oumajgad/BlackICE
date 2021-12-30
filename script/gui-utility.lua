0bd1e0fa27412ff8414b2b062f506810cc73b2e3
Dsafe1 <77176960+Dsafe1@users.noreply.github.com>
2021-12-30 11:49:19 +0100

    Gui updates

--- script/gui-utility.lua
+++ script/gui-utility.lua
@@ -112,8 +112,8 @@ if wx ~= nil then
 
 	UI.gSizer2:Add( UI.m_textCtrl_ResEff, 0, wx.wxALIGN_CENTER + wx.wxALL, 5 )
 
-	UI.m_staticText34 = wx.wxStaticText( UI.m_panel_C_Info, wx.wxID_ANY, "Monthly war exhaustion change", wx.wxDefaultPosition, wx.wxDefaultSize, 0 )
-	UI.m_staticText34:Wrap( -1 )
+	UI.m_staticText34 = wx.wxStaticText( UI.m_panel_C_Info, wx.wxID_ANY, "Monthly war exhaustion change\n\nYou can only gain WE during wartime, regardless of what this value is.", wx.wxDefaultPosition, wx.wxDefaultSize, wx.wxALIGN_CENTER_HORIZONTAL )
+	UI.m_staticText34:Wrap( 200 )
 
 	UI.gSizer2:Add( UI.m_staticText34, 0, wx.wxALIGN_CENTER + wx.wxALL, 5 )
 
