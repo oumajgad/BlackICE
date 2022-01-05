efa728f92ffb33a21b5c4abe7c0e3ef151ba24ba
Dsafe1 <77176960+Dsafe1@users.noreply.github.com>
2022-01-05 18:17:53 +0100

    fix duplicate events; add duplicate check to EventIDs.py

--- DaveStuff/EventIDs.py
+++ DaveStuff/EventIDs.py
@@ -8,7 +8,9 @@ for root, dirs, files in os.walk(folder):
     for file in files:
         with open(os.path.join(folder, file), "r") as eventfile:
             for line in eventfile:
-                if "id =" in line:
+                if line.startswith("#"):
+                    continue
+                if "id =" in line and not "province_id" in line:
                     split_line = line.strip().split("=")[1]
                     if "#" in split_line:
                         IDs.append(split_line.strip().split("#")[0])
@@ -40,8 +42,13 @@ for ID in range(CIDs[0], CIDs[-1]):
         if a % 20 == 0:
             FreeID.append("\n \n")
 
-
 print(*FreeID)
 print("Above is a list of the next 1000 free event numbers, in blocks of 20.")
 print("There are currently " + str(len(CIDs)) + " Events in the Game.")
 print("Check the file for taken ID numbers!")
+  
+seen = set()
+for x in CIDs:
+    if x in seen: 
+        print(f"Duplicate ID - {x}")
+    seen.add(x)
