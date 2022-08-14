06654e0558bbfcc6d29675b7b69cbdb7fc2e03a4
Dsafe1 <77176960+Dsafe1@users.noreply.github.com>
2022-08-14 22:59:02 +0200

    Update division.py

--- tools/DivisionBuilder/division.py
+++ tools/DivisionBuilder/division.py
@@ -1,6 +1,7 @@
 from brigade import Brigade
 from utils import merge_dicts_and_add, divide_dict
 from collections import OrderedDict
+from copy import deepcopy
 
 
 class Division:
@@ -25,19 +26,29 @@ class Division:
 
     def calculate_stats(self):
         self.division_stats = dict()
+
+        if len(self.brigades) == 0:
+            self.division_stats_ordered = dict()
+            return
+
         self.division_stats["build_cost_ic"] = self.calculate_ic_cost()
+
         for brigade in self.brigades:
             s: dict = dict(brigade.current_stats)
+
             if s["maximum_speed"] < self.division_stats.get("maximum_speed", 10000):
                 self.division_stats["maximum_speed"] = s["maximum_speed"]
             if s["build_time"] > self.division_stats.get("build_time", 0):
                 self.division_stats["build_time"] = s["build_time"]
+
             s.pop("maximum_speed")
             s.pop("build_time")
             self.division_stats = merge_dicts_and_add(self.division_stats, s)
+
         for key in self.division_stats:
             if isinstance(self.division_stats[key], dict):  # Terrain effects
                 self.division_stats[key] = divide_dict(self.division_stats[key], len(self.brigades))
+
         self.division_stats["default_morale"] = round(self.division_stats["default_morale"] / len(self.brigades), 3)
         self.division_stats["default_organisation"] = round(self.division_stats["default_organisation"] /
                                                             len(self.brigades), 3)
