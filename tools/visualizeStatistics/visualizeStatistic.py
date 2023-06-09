06733a3952480eb7b2ae2376833821513dc6718e
Dsafe1 <77176960+Dsafe1@users.noreply.github.com>
2023-06-09 22:31:04 +0200

    Update visualizeStatistic.py

--- tools/visualizeStatistics/visualizeStatistic.py
+++ tools/visualizeStatistics/visualizeStatistic.py
@@ -10,12 +10,34 @@ import os
 # Place the script within the Stats folder and run
 #
 
-def plot_line(tag = None, stat = None, forceSameChart = False):
-    if not tag or not stat:
-        tag, stat = verify(forceSameChart)
+def getTag():
+    return e_TAG.get()
 
+def getStats():
+    stats = [lb_STAT.get(i) for i in lb_STAT.curselection()]
+    return stats
+
+def plot():
+    if same_chart.get() == 0:
+        plt.figure()
+
+    tag = getTag()
+    stats = getStats()
     if not tag:
         return
+    if not stats:
+        return
+
+    for stat in stats:
+        if verify(tag=tag, stat=stat):
+            plot_line(tag=tag,stat=stat)
+
+    plt.show()
+
+
+def plot_line(tag, stat, title=None):
+    if not title:
+        title = stat
     
     data = pd.read_csv(f'{tag}_{stat}')
     data = removeduplicates(data, [stat])
@@ -29,31 +51,6 @@ def plot_line(tag = None, stat = None, forceSameChart = False):
     plt.ylim(0)
     plt.title(stat)
     plt.autoscale()
-    plt.show()
-
-def plot_line_no_show(tag = None, stat = None, forceSameChart = False, title=None):
-
-    if not title:
-        title = stat
-    if not tag or not stat:
-        tag, stat = verify(forceSameChart)
-    else:
-        verify(forceSameChart)
-
-    if not tag:
-        return
-    
-    data = pd.read_csv(f'{tag}_{stat}')
-    data = removeduplicates(data, [stat])
-    dates = data["Date"]
-    plt.plot(dates, data[stat], label=f"{tag}-{stat}")
-    plt.legend()
-    plt.xlabel("Day")
-    plt.ylabel(title)
-    plt.grid(visible=True)
-    plt.xlim(0)
-    plt.ylim(0)
-    plt.title(title)
 
 
 # Removes duplicate dates from the dataset which can be introduced due to crashing or save reloading
@@ -82,45 +79,12 @@ def removeduplicates(data: pd.DataFrame, columns: list[str]):
     return vals
 
 
-def verify(forceSameChart):
-    tag = e_TAG.get()
-    stat = lb_STAT.get(lb_STAT.curselection())
-    if same_chart.get() == 0 and not forceSameChart:
-        plt.figure()
+def verify(tag, stat):
     if len(tag) != 3:
         return False
     if os.path.exists(f"{tag}_{stat}"):
-        return tag, stat
+        return True
     return False
-
-
-def quickStart():
-    tag = e_TAG.get()
-    if len(tag) != 3:
-        return
-    plot_line_no_show(tag, "domSpy", False)
-    plot_line_no_show(tag, "FreeSpies", True, "Spies")
-
-    plot_line_no_show(tag, "TotalSpiesAbroad", False)
-
-    plot_line_no_show(tag, "PercentEspionage", False)
-
-    # plot_line_no_show(tag, "Popularity_fascistic", False)
-    # plot_line_no_show(tag, "Popularity_left_wing_radical", True)
-    # plot_line_no_show(tag, "Popularity_leninist", True)
-    # plot_line_no_show(tag, "Popularity_market_liberal", True)
-    # plot_line_no_show(tag, "Popularity_national_socialist", True)
-    # plot_line_no_show(tag, "Popularity_paternal_autocrat", True)
-    # plot_line_no_show(tag, "Popularity_social_conservative", True)
-    # plot_line_no_show(tag, "Popularity_social_democrat", True)
-    # plot_line_no_show(tag, "Popularity_social_liberal", True)
-    # plot_line_no_show(tag, "Popularity_stalinist", True, "Popularity")
-
-    plot_line_no_show(tag, "Popularity_Group_communism", False)
-    plot_line_no_show(tag, "Popularity_Group_democracy", True)
-    plot_line_no_show(tag, "Popularity_Group_fascism", True, "Popularity Group")
-    plt.autoscale()
-    plt.show()
 
 
 def setPossibleStatSelection():
@@ -143,23 +107,21 @@ app.title("visualizeStatistics.py")
 e_TAG = tk.Entry(app, width=25, border=5)
 e_TAG.insert(0, "Enter TAG")
 
-lb_STAT = tk.Listbox(app, selectmode=tk.SINGLE, width=40, height=20)
+lb_STAT = tk.Listbox(app, selectmode=tk.EXTENDED, width=40, height=20)
 
 same_chart = tk.IntVar()
 same_chart.set(0)
 cb = tk.Checkbutton(app, text="Same Chart", variable=same_chart, onvalue=1, offvalue=0)
 
 
-button_ShowLine = tk.Button(app, text="Show Line", width=25, command= lambda: plot_line())
-button_QuickStart = tk.Button(app, text="Party & Spies", width=25, command= lambda: quickStart())
+button_Show = tk.Button(app, text="Plot", width=25, command= lambda: plot())
 
 
 
 e_TAG.grid(row=0, column=0, padx=10, pady=10)
 lb_STAT.grid(row=0, column=1, padx=10, pady=10)
 cb.grid(row=0, column=2, padx=10, pady=10)
-button_ShowLine.grid(row=1, column=0, padx=10, pady=10)
-button_QuickStart.grid(row=1, column=1, padx=10, pady=10)
+button_Show.grid(row=1, column=1, padx=10, pady=10, )
 if __name__ == "__main__":
     setPossibleStatSelection()
     app.mainloop()
