dd9569a6e87eceb79f10e5bebe34ccb2006e917d
Dsafe1 <77176960+Dsafe1@users.noreply.github.com>
2023-06-15 20:40:31 +0200

    GUI

--- script/gui-utility-gameinfo.lua
+++ script/gui-utility-gameinfo.lua
@@ -67,6 +67,99 @@ if wx ~= nil then
 	UI.m_panel_GameInfo_Generals:Layout()
 	UI.bSizer_GameInfo2:Fit( UI.m_panel_GameInfo_Generals )
 	UI.m_notebook5:AddPage(UI.m_panel_GameInfo_Generals, "Generals", False )
+	UI.m_panel_GameInfo_Tech = wx.wxPanel( UI.m_notebook5, wx.wxID_ANY, wx.wxDefaultPosition, wx.wxDefaultSize, wx.wxTAB_TRAVERSAL )
+	UI.bSizer_GameInfo3 = wx.wxBoxSizer( wx.wxVERTICAL )
+
+	UI.m_choice_TechsChoices = {}
+	UI.m_choice_Techs = wx.wxChoice( UI.m_panel_GameInfo_Tech, wx.wxID_ANY, wx.wxDefaultPosition, wx.wxSize( 350,-1 ), UI.m_choice_TechsChoices, 0 )
+	UI.m_choice_Techs:SetSelection( 0 )
+	UI.bSizer_GameInfo3:Add( UI.m_choice_Techs, 0, wx.wxALIGN_CENTER + wx.wxALL + wx.wxEXPAND, 5 )
+
+	UI.gSizer_GameInfo_Techs_1 = wx.wxGridSizer( 1, 5, 0, 0 )
+
+	UI.m_staticText_GameInfo_Techs_1 = wx.wxStaticText( UI.m_panel_GameInfo_Tech, wx.wxID_ANY, "Your level", wx.wxDefaultPosition, wx.wxDefaultSize, 0 )
+	UI.m_staticText_GameInfo_Techs_1:Wrap( -1 )
+
+	UI.gSizer_GameInfo_Techs_1:Add( UI.m_staticText_GameInfo_Techs_1, 0, wx.wxALIGN_CENTER + wx.wxALL, 5 )
+
+	UI.m_textCtrl_GameInfo_Techs_1 = wx.wxTextCtrl( UI.m_panel_GameInfo_Tech, wx.wxID_ANY, "", wx.wxDefaultPosition, wx.wxSize( 50,-1 ), 0 )
+	UI.m_textCtrl_GameInfo_Techs_1:Enable( False )
+
+	UI.gSizer_GameInfo_Techs_1:Add( UI.m_textCtrl_GameInfo_Techs_1, 0, wx.wxALIGN_CENTER + wx.wxALL, 5 )
+
+	UI.m_staticText_GameInfo_Techs_2 = wx.wxStaticText( UI.m_panel_GameInfo_Tech, wx.wxID_ANY, "Shown level", wx.wxDefaultPosition, wx.wxDefaultSize, 0 )
+	UI.m_staticText_GameInfo_Techs_2:Wrap( -1 )
+
+	UI.gSizer_GameInfo_Techs_1:Add( UI.m_staticText_GameInfo_Techs_2, 0, wx.wxALIGN_CENTER + wx.wxALL, 5 )
+
+	UI.m_textCtrl_GameInfo_Techs_2 = wx.wxTextCtrl( UI.m_panel_GameInfo_Tech, wx.wxID_ANY, "", wx.wxDefaultPosition, wx.wxSize( 50,-1 ), 0 )
+	UI.m_textCtrl_GameInfo_Techs_2:Enable( False )
+
+	UI.gSizer_GameInfo_Techs_1:Add( UI.m_textCtrl_GameInfo_Techs_2, 0, wx.wxALIGN_CENTER + wx.wxALL, 5 )
+
+	UI.gSizer47 = wx.wxGridSizer( 0, 2, 0, 0 )
+
+	UI.m_button_GameInfo_Tech_increase = wx.wxButton( UI.m_panel_GameInfo_Tech, wx.wxID_ANY, "+", wx.wxDefaultPosition, wx.wxSize( 25,-1 ), 0 )
+	UI.gSizer47:Add( UI.m_button_GameInfo_Tech_increase, 0, wx.wxALIGN_CENTER + wx.wxALL, 5 )
+
+	UI.m_button_GameInfo_Tech_decrease = wx.wxButton( UI.m_panel_GameInfo_Tech, wx.wxID_ANY, "-", wx.wxDefaultPosition, wx.wxSize( 25,-1 ), 0 )
+	UI.gSizer47:Add( UI.m_button_GameInfo_Tech_decrease, 0, wx.wxALIGN_CENTER + wx.wxALL, 5 )
+
+
+	UI.gSizer_GameInfo_Techs_1:Add( UI.gSizer47, 0, wx.wxEXPAND, 5 )
+
+
+	UI.bSizer_GameInfo3:Add( UI.gSizer_GameInfo_Techs_1, 0, wx.wxEXPAND, 5 )
+
+	UI.gSizer_GameInfo_Techs_2 = wx.wxGridSizer( 1, 2, 0, 0 )
+
+	UI.fgSizer_GameInfo_Techs_1 = wx.wxFlexGridSizer( 3, 1, 0, 0 )
+	UI.fgSizer_GameInfo_Techs_1:AddGrowableCol( 0 )
+	UI.fgSizer_GameInfo_Techs_1:AddGrowableRow( 1 )
+	UI.fgSizer_GameInfo_Techs_1:SetFlexibleDirection( wx.wxBOTH )
+	UI.fgSizer_GameInfo_Techs_1:SetNonFlexibleGrowMode( wx.wxFLEX_GROWMODE_SPECIFIED )
+
+	UI.m_staticText_GameInfo_Techs_3 = wx.wxStaticText( UI.m_panel_GameInfo_Tech, wx.wxID_ANY, "Triggers", wx.wxDefaultPosition, wx.wxDefaultSize, 0 )
+	UI.m_staticText_GameInfo_Techs_3:Wrap( -1 )
+
+	UI.fgSizer_GameInfo_Techs_1:Add( UI.m_staticText_GameInfo_Techs_3, 0, wx.wxALIGN_CENTER + wx.wxALL, 5 )
+
+	UI.m_textCtrl_GameInfo_Techs_4 = wx.wxTextCtrl( UI.m_panel_GameInfo_Tech, wx.wxID_ANY, "", wx.wxDefaultPosition, wx.wxDefaultSize, wx.wxTE_MULTILINE )
+	UI.fgSizer_GameInfo_Techs_1:Add( UI.m_textCtrl_GameInfo_Techs_4, 1, wx.wxALL + wx.wxEXPAND, 5 )
+
+	UI.m_staticText_GameInfo_Techs_4 = wx.wxStaticText( UI.m_panel_GameInfo_Tech, wx.wxID_ANY, "Note that triggers may be combined. E.g. 2 \"OR\" clauses can be combined and show as 1 clause with multiple grouped conditions. This only happens if the 2 clauses are identical in the code, i.e.: \"or\" and \"OR\" will not be combined.", wx.wxDefaultPosition, wx.wxDefaultSize, 0 )
+	UI.m_staticText_GameInfo_Techs_4:Wrap( 230 )
+
+	UI.fgSizer_GameInfo_Techs_1:Add( UI.m_staticText_GameInfo_Techs_4, 0, wx.wxALL, 5 )
+
+
+	UI.gSizer_GameInfo_Techs_2:Add( UI.fgSizer_GameInfo_Techs_1, 1, wx.wxEXPAND, 5 )
+
+	UI.fgSizer_GameInfo_Techs_2 = wx.wxFlexGridSizer( 2, 1, 0, 0 )
+	UI.fgSizer_GameInfo_Techs_2:AddGrowableCol( 0 )
+	UI.fgSizer_GameInfo_Techs_2:AddGrowableRow( 1 )
+	UI.fgSizer_GameInfo_Techs_2:SetFlexibleDirection( wx.wxBOTH )
+	UI.fgSizer_GameInfo_Techs_2:SetNonFlexibleGrowMode( wx.wxFLEX_GROWMODE_SPECIFIED )
+
+	UI.m_staticText_GameInfo_Techs_5 = wx.wxStaticText( UI.m_panel_GameInfo_Tech, wx.wxID_ANY, "Effects", wx.wxDefaultPosition, wx.wxDefaultSize, 0 )
+	UI.m_staticText_GameInfo_Techs_5:Wrap( -1 )
+
+	UI.fgSizer_GameInfo_Techs_2:Add( UI.m_staticText_GameInfo_Techs_5, 0, wx.wxALIGN_CENTER + wx.wxALL, 5 )
+
+	UI.m_textCtrl_GameInfo_Techs_5 = wx.wxTextCtrl( UI.m_panel_GameInfo_Tech, wx.wxID_ANY, "", wx.wxDefaultPosition, wx.wxDefaultSize, wx.wxTE_MULTILINE )
+	UI.fgSizer_GameInfo_Techs_2:Add( UI.m_textCtrl_GameInfo_Techs_5, 1, wx.wxALL + wx.wxEXPAND, 5 )
+
+
+	UI.gSizer_GameInfo_Techs_2:Add( UI.fgSizer_GameInfo_Techs_2, 1, wx.wxEXPAND, 5 )
+
+
+	UI.bSizer_GameInfo3:Add( UI.gSizer_GameInfo_Techs_2, 1, wx.wxEXPAND, 5 )
+
+
+	UI.m_panel_GameInfo_Tech:SetSizer( UI.bSizer_GameInfo3 )
+	UI.m_panel_GameInfo_Tech:Layout()
+	UI.bSizer_GameInfo3:Fit( UI.m_panel_GameInfo_Tech )
+	UI.m_notebook5:AddPage(UI.m_panel_GameInfo_Tech, "Techs", False )
 
 
 	UI.MyFrame4 .m_mgr:Update()
