5c6464f38b2e362bb1a27f60999c2ec139faaf41
Dsafe1 <77176960+Dsafe1@users.noreply.github.com>
2025-05-24 18:39:22 +0200

    BiceLib.dll: remove usage of external for country vars/flags

--- DaveStuff/luabinaries/bice/BiceLib/BiceLib/GameClasses/CCountry.cpp
+++ DaveStuff/luabinaries/bice/BiceLib/BiceLib/GameClasses/CCountry.cpp
@@ -4,15 +4,15 @@
 
 uintptr_t CCountry::CountryPtrs[300];
 
-void CCountry::traverseFlagsAndVarTreeDepthFirst(Memory::External& external, std::vector<std::uintptr_t>* res, uintptr_t nodePtr) {
+void CCountry::traverseFlagsAndVarTreeDepthFirst(std::vector<std::uintptr_t>* res, uintptr_t nodePtr) {
     if (nodePtr == 0) {
         return;
     }
-    uintptr_t element = external.read<uintptr_t>(nodePtr);
-    char character = external.read<char>(nodePtr + 0x4);
-    uintptr_t parent = external.read<uintptr_t>(nodePtr + 0x8);
-    uintptr_t sibling = external.read<uintptr_t>(nodePtr + 0xC);
-    uintptr_t child = external.read<uintptr_t>(nodePtr + 0x10);
+    uintptr_t element = *(uintptr_t*)(nodePtr);
+    char character = *(char*)(nodePtr + 0x4);
+    uintptr_t parent = *(uintptr_t*)(nodePtr + 0x8);
+    uintptr_t sibling = *(uintptr_t*)(nodePtr + 0xC);
+    uintptr_t child = *(uintptr_t*)(nodePtr + 0x10);
     //std::cout << "nodePtr: " << Memory::n2hexstr(nodePtr) 
     //    << " char: " << character 
     //    << " element: " << Memory::n2hexstr(element) 
@@ -21,7 +21,7 @@ void CCountry::traverseFlagsAndVarTreeDepthFirst(Memory::External& external, std
     //    << " child: " << Memory::n2hexstr(child) << std::endl;
     if (parent != 0) {
         //std::cout << "parent" << std::endl;
-        CCountry::traverseFlagsAndVarTreeDepthFirst(external, res, parent);
+        CCountry::traverseFlagsAndVarTreeDepthFirst(res, parent);
     }
 
     if (element != 0) {
@@ -30,26 +30,26 @@ void CCountry::traverseFlagsAndVarTreeDepthFirst(Memory::External& external, std
     }
     if (child != 0) {
         //std::cout << "child" << std::endl;
-        CCountry::traverseFlagsAndVarTreeDepthFirst(external, res, child);
+        CCountry::traverseFlagsAndVarTreeDepthFirst(res, child);
     }
     if (sibling != 0) {
         //std::cout << "sibling " << sibling << std::endl;
-        CCountry::traverseFlagsAndVarTreeDepthFirst(external, res, sibling);
+        CCountry::traverseFlagsAndVarTreeDepthFirst(res, sibling);
     }
 }
 
-std::vector<std::pair<std::string, std::string>> CCountry::getActiveEventModifiers(Memory::External& external, uintptr_t countryPtr) {
+std::vector<std::pair<std::string, std::string>> CCountry::getActiveEventModifiers(uintptr_t countryPtr) {
     std::vector<std::pair<std::string,std::string>> res;
-    uintptr_t listNodePtr = external.read<uintptr_t>(countryPtr + 0x648);
+    uintptr_t listNodePtr = *(uintptr_t*)(countryPtr + 0x648);
     while (listNodePtr != 0) {
         HDS::LinkedListNodeSingle* listNode = (HDS::LinkedListNodeSingle*)listNodePtr;
 
         auto modifierPtr = listNode->data;
-        auto staticModifier = external.read<uintptr_t>(modifierPtr + 0x8);
+        auto staticModifier = *(uintptr_t*)(modifierPtr + 0x8);
 
         auto name = std::string(utils::getCString((DWORD*)(staticModifier + 0x2c)));
 
-        int expiryDateTick = external.read<int>(modifierPtr + 0xC);
+        int expiryDateTick = *(int*)(modifierPtr + 0xC);
         auto expiryDate = utils::gameTickToDate(expiryDateTick);
 
         res.push_back(std::make_pair(name, expiryDate));
@@ -59,13 +59,13 @@ std::vector<std::pair<std::string, std::string>> CCountry::getActiveEventModifie
     return res;
 }
 
-std::vector<std::string>* CCountry::getFlags(Memory::External& external, uintptr_t countryPtr) {
+std::vector<std::string>* CCountry::getFlags(uintptr_t countryPtr) {
     std::vector<std::uintptr_t>* ptrs = new std::vector<std::uintptr_t>;
 
     uintptr_t flagsOffset = countryPtr + 0x180 + 0x4; // CFlagsVFTable + Flag Tree beginning
-    uintptr_t flagsPtr = external.read<uintptr_t>(flagsOffset);
+    uintptr_t flagsPtr = *(uintptr_t*)(flagsOffset);
 
-    CCountry::traverseFlagsAndVarTreeDepthFirst(external, ptrs, flagsPtr);
+    CCountry::traverseFlagsAndVarTreeDepthFirst(ptrs, flagsPtr);
     std::vector<std::string>* res = new std::vector<std::string>;
     for (auto& i : *ptrs) {
         std::string x = std::string(utils::getCString((DWORD*)i));
@@ -75,18 +75,18 @@ std::vector<std::string>* CCountry::getFlags(Memory::External& external, uintptr
     return res;
 }
 
-std::vector<HDS::CVariable>* CCountry::getVars(Memory::External& external, uintptr_t countryPtr) {
+std::vector<HDS::CVariable>* CCountry::getVars(uintptr_t countryPtr) {
     std::vector<std::uintptr_t>* ptrs = new std::vector<std::uintptr_t>;
 
     uintptr_t varsOffset = countryPtr + 0x1AC + 0x4; // CVariablesVFTable + Vars Tree beginning
-    uintptr_t varsPtr = external.read<uintptr_t>(varsOffset);
+    uintptr_t varsPtr = *(uintptr_t*)varsOffset;
 
-    CCountry::traverseFlagsAndVarTreeDepthFirst(external, ptrs, varsPtr);
+    CCountry::traverseFlagsAndVarTreeDepthFirst(ptrs, varsPtr);
     std::vector<HDS::CVariable>* res = new std::vector<HDS::CVariable>;
     for (auto& i : *ptrs) {
         HDS::CVariable x;
         x.name = std::string(utils::getCString((DWORD*)i));
-        x.value = external.read<int32_t>(i + 0x1C);
+        x.value = *(int32_t*)(i + 0x1C);
         if (x.value != 0) {
             res->push_back(x);
         }
