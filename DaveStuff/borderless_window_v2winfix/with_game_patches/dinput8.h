fa85fbd0ae2cae1db8335da6a638dd2ef2f9c497
Dsafe1 <77176960+Dsafe1@users.noreply.github.com>
2025-05-31 23:13:58 +0200

    remove other dinput8 projects

--- DaveStuff/borderless_window_v2winfix/with_game_patches/dinput8.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-typedef HRESULT(WINAPI *tDirectInput8Create)(HINSTANCE inst_handle, DWORD version, const IID& r_iid, LPVOID* out_wrapper, LPUNKNOWN p_unk);
-extern tDirectInput8Create oDirectInput8Create;
