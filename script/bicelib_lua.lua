ce3649c1df87c82afcb8945b67a320aacb1d728c
Dsafe1 <77176960+Dsafe1@users.noreply.github.com>
2025-06-23 16:39:57 +0200

    set default CoC unit limit at startup

--- script/bicelib_lua.lua
+++ script/bicelib_lua.lua
@@ -37,9 +37,9 @@ if mod then
     -- BiceLib.Leaders.checkRankSpecificTraitsConsistency()
 
     -- Units
-    -- BiceLib.Units.setCorpsUnitLimit(6, "---")
-    -- BiceLib.Units.setArmyUnitLimit(7, "---")
-    -- BiceLib.Units.setArmyGroupUnitLimit(8, "---")
+    BiceLib.Units.setCorpsUnitLimit(4, "---")
+    BiceLib.Units.setArmyUnitLimit(4, "---")
+    BiceLib.Units.setArmyGroupUnitLimit(4, "---")
     -- BiceLib.Units.setArmyGroupUnitLimit(10, "GER")
     -- BiceLib.Units.addCommandLimitTrait("pskill_1", -1)
     -- BiceLib.Units.addCommandLimitTrait("pskill_4", 1)
