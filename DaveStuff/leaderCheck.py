a279b7b8dc469769bfd5017aedb4fb4a483abaea
Dsafe1 <77176960+Dsafe1@users.noreply.github.com>
2025-12-14 16:19:59 +0100

    Update leaderCheck.py

--- DaveStuff/leaderCheck.py
+++ DaveStuff/leaderCheck.py
@@ -2,7 +2,7 @@ import os
 
 
 
-leaders = list()
+leaderIDs = list()
 for root, dirs, files in os.walk("./history/leaders"):
     for file1 in files:
         with open(root + "/" + file1 , "r", errors="ignore") as file:
@@ -14,7 +14,7 @@ for root, dirs, files in os.walk("./history/leaders"):
                     x += 1
                     leaderID = line.split("=")[0].strip()
                     #print(leaderID)
-                    leaders.append(int(leaderID))
+                    leaderIDs.append(int(leaderID))
                     continue
                 if "{" in line:
                     x += 1
@@ -23,28 +23,54 @@ for root, dirs, files in os.walk("./history/leaders"):
 
 leadersSet = set()
 duplicates = set()
-for l in leaders:
+for l in leaderIDs:
     if l not in leadersSet:
         leadersSet.add(l)
     else:
         duplicates.add(l)
 
 
-leaders.sort()
-freeleaders = []
-y = 0
-for l in range(leaders[0], leaders[-1]):
-    if y >= 1000:
-        break
-    if l not in leaders:
-        freeleaders.append(l)
-        y += 1
-        if y % 20 == 0:
-            freeleaders.append("\n \n")
+leaderIDs.sort()
 
-print(*freeleaders)
-print("Above is a list of the next 1000 free leader numbers, in blocks of 20.")
-print("There are currently " + str(len(leadersSet)) + " leaders in the Game.")
+# nextFreeLeaders = []
+# y = 0
+# for l in range(leaderIDs[0], leaderIDs[-1]):
+#     if y >= 1000:
+#         break
+#     if l not in leaderIDs:
+#         nextFreeLeaders.append(l)
+#         y += 1
+#         if y % 20 == 0:
+#             nextFreeLeaders.append("\n \n")
+
+# print(*nextFreeLeaders)
+# print("Above is a list of the next 1000 free leader numbers, in blocks of 20.")
+# print("There are currently " + str(len(leadersSet)) + " leaders in the Game.")
+
+freeLeaderBlocks = []
+currentBlock = {
+    "start": None,
+    "end": 0
+}
+for potentialLeaderId in range(leaderIDs[0], 100_000):
+    if potentialLeaderId not in leaderIDs:
+        if currentBlock["start"] == None:
+            currentBlock["start"] = potentialLeaderId
+        else:
+            currentBlock["end"] = potentialLeaderId
+    else:
+        if currentBlock["start"] == None:
+            continue
+        if currentBlock["end"] - currentBlock["start"] >= 100:
+            freeLeaderBlocks.append(currentBlock)
+            currentBlock = {
+                "start": None,
+                "end": 0
+            }
+
+print("Ranges of at least 100 free leader IDs")
+for freeLeaderBlock in freeLeaderBlocks:
+    print(f"Amount: {freeLeaderBlock['end'] - freeLeaderBlock['start']} Start: {freeLeaderBlock['start']} End: {freeLeaderBlock['end']}")
 
 if len(duplicates) >= 1:
     print("DUPLICATES")
