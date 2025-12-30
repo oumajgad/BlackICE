382066c1f866480bf73f708e87b2b6f4f809921e
Dsafe1 <77176960+Dsafe1@users.noreply.github.com>
2025-12-30 21:01:52 +0100

    update leader death generation scripts (2/2)

--- tools/leader_deaths_event/create_events_from_json.py
+++ tools/leader_deaths_event/create_events_from_json.py
@@ -28,18 +28,24 @@ with open("tools\leader_deaths_event\leader_deaths_sorted.json", "r") as f:
 
 event_id = 78000
 write_lines = []
+handled_leaders = []
 for tag, dates in leader_deaths.items():
     write_lines.append(header_template.format(tag=tag))
     for date, leaders in dates.items():
-        print(tag)
-        print(date)
-        print(leaders)
+        # print(tag)
+        # print(date)
+        # print(leaders)
         event_lines = ""
         for leader in leaders:
             event_lines = event_lines + (f"\n        kill_leader = {leader}")
+            if leader not in handled_leaders:
+                handled_leaders.append(leader)
+            else:
+                print(f"Duplicate leader death in JSON. Leader ID: {leader}")
         event_text = event_template.format(event_id=event_id, tag=tag, date=date, text=event_lines)
         event_id += 1
         write_lines.append(event_text)
 
 with open("tools\leader_deaths_event\generated_leader_death_events.txt", "w") as f:
     f.writelines(write_lines)
+
