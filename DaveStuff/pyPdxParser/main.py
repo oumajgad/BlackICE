08fed1000cda75d3d7873f6207bc5056bc41bf88
Dsafe1 <77176960+Dsafe1@users.noreply.github.com>
2026-03-28 15:04:01 +0100

    relationship impact for strat trades

--- DaveStuff/pyPdxParser/main.py
+++ DaveStuff/pyPdxParser/main.py
@@ -6,22 +6,21 @@ from parser.node import Node
 from tqdm import tqdm
 
 if __name__ == "__main__":
-    path = r"C:\Users\David\GitHub\BlackICE\events"
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            print(file)
-            x = parse_file(Path(root).joinpath(Path(file)))
-            # print(x)
-            x_node = Node.from_parsed(parsed=x)
-            # print(x_node)
-            nodes = x_node.find_by_key(key="controls")
-            for node in nodes:
-                event = node.get_parent_by_key("country_event")
-                if event:
-                    event_id = event.find_by_key("id")
-                    if len(event_id) > 0:
-                        print(event_id[0].scalar_value)
-                    else:
-                        print("event id not found")
-                else:
-                    print("event is null")
+    base_path = r"C:\Users\David\GitHub\BlackICE"
+    for path in [
+        rf"{base_path}\common\countries",
+        rf"{base_path}\history",
+        rf"{base_path}\decisions",
+        rf"{base_path}\events",
+    ]:
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                if file == "credits.txt":
+                    continue
+                print(Path(root).joinpath(Path(file)))
+                x = parse_file(Path(root).joinpath(Path(file)))
+                # print(x)
+                x_node = Node.from_parsed(parsed=x)
+                colors = x_node.find_by_key("color")
+                print(file)
+                # print(x_node)
